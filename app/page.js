17cf629409ee0455a80010e874a7843bbca2c4c1
Yashvi Sharma <137611141+yashvisharma1204@users.noreply.github.com>
2024-08-01 20:28:49 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-'use client';
+
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowPointer } from '@fortawesome/free-solid-svg-icons';
